8b269c53797afb665d1e72aa659406442b1b0cdb
Melany Delgado <melany.delgado@edify.cr>
2016-10-28 09:13:25 -0600

    Assignment Table Integration Test

--- tests/integration/components/gru-assignments-table-test.js
+++ tests/integration/components/gru-assignments-table-test.js
@@ -1,24 +1,50 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import Ember from 'ember';
 
 moduleForComponent('gru-assignments-table', 'Integration | Component | gru assignments table', {
   integration: true
 });
 
 test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+  var assignments = Ember.A([
+    Ember.Object.create({
+      hasStarted:true,
+      score:60,
+      title:'Assessment 1',
+      standards:'',
+      assignedDate:'27/10/2016',
+      dueDate:'2/11,2016',
+      totalAttempts:15,
+      attempts:2
+    }),
+    Ember.Object.create({
+      hasStarted:true,
+      score:10,
+      title:'Assessment 2',
+      standards:'',
+      assignedDate:'27/10/2016',
+      dueDate:'3/11,2016',
+      totalAttempts:15,
+      attempts:2
+    })]);
+  this.set('assignments',assignments);
 
-  this.render(hbs`{{gru-assignments-table}}`);
+  this.render(hbs`{{gru-assignments-list assignments=assignments}}`);
 
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#gru-assignments-table}}
-      template block text
-    {{/gru-assignments-table}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  var $assignmentTable = this.$();
+  assert.ok($assignmentTable.find('.gru-assignments-table table').length, 'Missing table');
+  assert.ok($assignmentTable.find('.gru-assignments-table table thead tr th.num-header').length, 'Missing Num Header');
+  assert.ok($assignmentTable.find('.gru-assignments-table table thead tr th.score-header').length, 'Missing Score Header');
+  assert.ok($assignmentTable.find('.gru-assignments-table table thead tr th.title-header').length, 'Missing title Header');
+  assert.ok($assignmentTable.find('.gru-assignments-table table thead tr th.standards-header').length, 'Missing standards Header');
+  assert.ok($assignmentTable.find('.gru-assignments-table table thead tr th.date-header').length, 'Missing Due date Header');
+  assert.ok($assignmentTable.find('.gru-assignments-table table thead tr th.attempts-header').length, 'Missing Attempts Header');
+  assert.equal($assignmentTable.find('.gru-assignments-table table tbody tr').length,2, 'Should have 2 rows');
+  assert.ok($assignmentTable.find('.gru-assignments-table table tbody tr td.num').length,'Missing num column');
+  assert.ok($assignmentTable.find('.gru-assignments-table table tbody tr td.score').length,'Missing score column');
+  assert.ok($assignmentTable.find('.gru-assignments-table table tbody tr td.title').length,'Missing title column');
+  assert.ok($assignmentTable.find('.gru-assignments-table table tbody tr td.standards').length,'Missing standards column');
+  assert.ok($assignmentTable.find('.gru-assignments-table table tbody tr td.due-date').length,'Missing due date column');
+  assert.ok($assignmentTable.find('.gru-assignments-table table tbody tr td.attempts').length,'Missing attempts column');
 });

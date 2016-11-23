b9f83f8f23cee05936b25e4c28fabeef65a075af
Melany Delgado <melany.delgado@edify.cr>
2016-11-23 15:17:04 -0600

    List of assignments for teachers: mobile view

--- tests/integration/components/gru-assignments-table-test.js
+++ tests/integration/components/gru-assignments-table-test.js
@@ -39,10 +39,12 @@ test('Table layout student', function(assert) {
   assert.ok($assignmentTable.find('.gru-assignments-table table thead tr th.title-header').length, 'Missing title Header');
   assert.ok($assignmentTable.find('.gru-assignments-table table thead tr th.standards-header').length, 'Missing standards Header');
   assert.ok($assignmentTable.find('.gru-assignments-table table thead tr th.date-header').length, 'Missing Due date Header');
-  assert.ok($assignmentTable.find('.gru-assignments-table table thead tr th.attempts-header').length, 'Missing Attempts Header');
+  assert.ok($assignmentTable.find('.gru-assignments-table table thead tr th.attempts-header.hidden-xs').length, 'Missing Attempts Header');
+  assert.ok($assignmentTable.find('.gru-assignments-table table thead tr th.attempts-header.visible-xs').length, 'Missing Mobile Attempts Header');
   assert.notOk($assignmentTable.find('.gru-assignments-table table thead tr th.modified-date-header').length, 'Modified Date Header should not appear');
   assert.notOk($assignmentTable.find('.gru-assignments-table table thead tr th.students-assigned-header').length, 'Students Assigned Header should not appear');
   assert.notOk($assignmentTable.find('.gru-assignments-table table thead tr th.actions-header').length, 'Actions Header should not appear');
+  assert.ok($assignmentTable.find('.gru-assignments-table table thead tr th.options-header.visible-xs ').length, 'Missing Mobile Options Header');
   assert.equal($assignmentTable.find('.gru-assignments-table table tbody tr').length,2, 'Should have 2 rows');
   assert.ok($assignmentTable.find('.gru-assignments-table table tbody tr td.num').length,'Missing num column');
   assert.ok($assignmentTable.find('.gru-assignments-table table tbody tr td.score').length,'Missing score column');
@@ -51,6 +53,7 @@ test('Table layout student', function(assert) {
   assert.ok($assignmentTable.find('.gru-assignments-table table tbody tr td.standards').length,'Missing standards column');
   assert.ok($assignmentTable.find('.gru-assignments-table table tbody tr td.due-date').length,'Missing due date column');
   assert.ok($assignmentTable.find('.gru-assignments-table table tbody tr td.attempts').length,'Missing attempts column');
+  assert.ok($assignmentTable.find('.gru-assignments-table table tbody tr td.actions.visible-xs').length,'Missing mobile actions column');
   assert.notOk($assignmentTable.find('.gru-assignments-table table tbody tr td.modified-date').length,'Modify date  column should not appear');
   assert.notOk($assignmentTable.find('.gru-assignments-table table tbody tr td.students-assigned').length,'Students assigned  column should not appear');
   assert.notOk($assignmentTable.find('.gru-assignments-table table tbody tr td.actions button.add-student').length,'Add student button should not appear');
@@ -89,11 +92,13 @@ test('Table layout teacher', function(assert) {
   assert.notOk($assignmentTable.find('.gru-assignments-table table thead tr th.score-header').length, 'Score Header should not appear');
   assert.ok($assignmentTable.find('.gru-assignments-table table thead tr th.title-header').length, 'Missing title Header');
   assert.ok($assignmentTable.find('.gru-assignments-table table thead tr th.modified-date-header').length, 'Missing Modified Date Header');
-  assert.ok($assignmentTable.find('.gru-assignments-table table thead tr th.students-assigned-header').length, 'Missing Students Assigned Header');
+  assert.ok($assignmentTable.find('.gru-assignments-table table thead tr th.students-assigned-header.hidden-xs').length, 'Missing Students Assigned Header');
+  assert.ok($assignmentTable.find('.gru-assignments-table table thead tr th.students-assigned-header.visible-xs').length, 'Missing Mobile Students Assigned Header');
   assert.ok($assignmentTable.find('.gru-assignments-table table thead tr th.actions-header').length, 'Missing Actions Header');
   assert.notOk($assignmentTable.find('.gru-assignments-table table thead tr th.standards-header').length, 'Standards Header should not appear');
   assert.notOk($assignmentTable.find('.gru-assignments-table table thead tr th.date-header').length, 'Due date Header should not appear');
   assert.notOk($assignmentTable.find('.gru-assignments-table table thead tr th.attempts-header').length, 'Attempts Header should not appear');
+  assert.ok($assignmentTable.find('.gru-assignments-table table thead tr th.options-header.visible-xs ').length, 'Missing Mobile Options Header');
   assert.equal($assignmentTable.find('.gru-assignments-table table tbody tr').length,2, 'Should have 2 rows');
   assert.ok($assignmentTable.find('.gru-assignments-table table tbody tr td.num').length,'Missing num column');
   assert.notOk($assignmentTable.find('.gru-assignments-table table tbody tr td.score').length,'Score column should not appear');
@@ -105,5 +110,6 @@ test('Table layout teacher', function(assert) {
   assert.ok($assignmentTable.find('.gru-assignments-table table tbody tr td.students-assigned').length,'Missing students assigned  column');
   assert.ok($assignmentTable.find('.gru-assignments-table table tbody tr td.actions button.add-student').length,'Missing add student button');
   assert.ok($assignmentTable.find('.gru-assignments-table table tbody tr td.actions button.real-time').length,'Missing real time button');
+  assert.ok($assignmentTable.find('.gru-assignments-table table tbody tr td.actions.visible-xs').length,'Missing Mobile actions column');
 
 });

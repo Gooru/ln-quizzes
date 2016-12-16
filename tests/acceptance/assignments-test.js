121c17051011b791a7ef1f6e61a332405a2a63d4
Melany Delgado <melany.delgado@edify.cr>
2016-12-16 13:20:00 -0600

    Acceptance tests for Update Context functionality

--- tests/acceptance/assignments-test.js
+++ tests/acceptance/assignments-test.js
@@ -20,10 +20,35 @@ test('visiting assignments', function(assert) {
 
   andThen(function() {
     assert.equal(currentURL(), '/profile/profile-id?isTeacher=true');
+    var $table = find('.gru-assignments-table table');
+    assert.equal($table.find('td.students-assigned').text(),'1','Should have 1 students assigned');
     const $addStudent = find('button.add-student');
     click($addStudent);
     andThen(function () {
       T.exists(assert, find('.gru-assign-student-modal'), 'Missing assign student modal');
+      var $studentRosterTab = Ember.$('.gru-assign-students .nav-tabs .student-roster a');
+      click($studentRosterTab);
+      andThen(function () {
+        var $student = Ember.$('.gru-assign-students .students .student-list .list-group-item:eq(2)');
+        click($student);
+        andThen(function () {
+          assert.equal( Ember.$('.gru-assign-students .students .list-container .student-list .list-group-item.selected').length,2, 'One student should be selected');
+          assert.equal(Ember.$('.selected-count').text(),'Selected 2 of 3','Incorrect selected count');
+          fillIn(Ember.$('#date-dueDate'), '12/10/2200');
+          var $assignButton = Ember.$('button.assign-btn');
+          click($assignButton);
+          andThen(function () {
+            var $table = find('.gru-assignments-table table');
+            assert.equal($table.find('td.students-assigned').text(),'2','Should have 2 students assigned');
+            var $assignment = $table.find('tbody td:eq(1)');
+            click($assignment);
+            andThen(function () {
+              var $detail = find('.gru-assignments-detail');
+              assert.equal($detail.find('.due-date').text().trim(), 'Due Date: 11:30 pm 12/10/2200','Incorrect due date')
+            });
+          });
+        });
+      });
     });
   });
 });

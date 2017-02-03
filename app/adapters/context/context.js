29698925914451297c0a5994b5ba175731e91cca
Martin Mendez <gmmendezp@gmail.com>
2017-02-03 11:28:45 -0600

    QZ-435. Use token in context adapter.

--- app/adapters/context/context.js
+++ app/adapters/context/context.js
@@ -1,11 +1,23 @@
 import Ember from 'ember';
+import TokenMixin from 'quizzes/mixins/token';
 
-export default Ember.Object.extend({
+export default Ember.Object.extend(TokenMixin,  {
 
+  /**
+   * @property {Object} namespace base url for context endpoints
+   */
   namespace: '/quizzes/api/v1/context',
 
+  /**
+   * @property {Object} namespaceCollection base url for context collection endpoints
+   */
   namespaceCollection: '/quizzes/api/v1/contexts',
 
+  /**
+   * Creates a context
+   * @param {Object} assignment
+   * @returns {Promise}
+   */
   createContext: function(assignment) {
     const namespace = this.get('namespace');
     const url = `${namespace}`;
@@ -15,15 +27,18 @@ export default Ember.Object.extend({
       dataType: 'json',
       processData: false,
       data: JSON.stringify(assignment),
-      // TODO get real headers
-      headers: {
+      headers: Object.assign(this.get('headers'), {
         'profile-id': '7973e6c6-f0cd-4021-9c49-a0f9f26101b2',
         'lms-id': 'its_learning'
-      }
+      })
     };
     return Ember.$.ajax(url, options);
   },
 
+  /**
+   * Gets all the contexts assigned to the current student
+   * @returns {Promise}
+   */
   getContextsAssigned: function() {
     const namespace = this.get('namespaceCollection');
     const options = {
@@ -31,14 +46,19 @@ export default Ember.Object.extend({
       contentType: 'application/json; charset=utf-8',
       dataType: 'json',
       processData: false,
-      headers: {
-        'profile-id': '8856379a-a8c9-402d-b1b0-faae3b36d137',//student-3
+      headers: Object.assign(this.get('headers'), {
+        'profile-id': '8856379a-a8c9-402d-b1b0-faae3b36d137',
         'lms-id': 'its_learning'
-      }
+      })
     };
     const url = `${namespace}/assigned`;
     return Ember.$.ajax(url, options);
   },
+
+  /**
+   * Gets all contexts created by the current teacher
+   * @returns {Promise}
+   */
   getContextsCreated: function() {
     const namespace = this.get('namespaceCollection');
     const options = {
@@ -46,15 +66,20 @@ export default Ember.Object.extend({
       contentType: 'application/json; charset=utf-8',
       dataType: 'json',
       processData: false,
-      headers: {
+      headers: Object.assign(this.get('headers'), {
         'profile-id': '7973e6c6-f0cd-4021-9c49-a0f9f26101b2',
         'lms-id': 'its_learning'
-      }
+      })
     };
     const url = `${namespace}/created`;
     return Ember.$.ajax(url, options);
   },
 
+  /**
+   * Get data to show on the real time report
+   * @param {String} contextId
+   * @returns {Promise}
+   */
   getReportData: function(contextId) {
     const namespace = this.get('namespace');
     const options = {
@@ -62,15 +87,22 @@ export default Ember.Object.extend({
       contentType: 'application/json; charset=utf-8',
       dataType: 'json',
       processData: false,
-      headers: {
+      headers: Object.assign(this.get('headers'), {
         'profile-id': '7973e6c6-f0cd-4021-9c49-a0f9f26101b2',
         'lms-id': 'its_learning'
-      }
+      })
     };
     const url = `${namespace}/${contextId}/events`;
     return Ember.$.ajax(url, options);
   },
 
+  /**
+   * Save the previous resource response and current resource
+   * @param {String} resourceId current resource
+   * @param {String} contextId
+   * @param {Object} previousResource resource to save
+   * @returns {Promise}
+   */
   moveToResource: function(resourceId, contextId, previousResource) {
     const namespace = this.get('namespace');
     let data = previousResource ? { previousResource } : {};
@@ -79,17 +111,21 @@ export default Ember.Object.extend({
       contentType: 'application/json; charset=utf-8',
       processData: false,
       data: JSON.stringify(data),
-      // TODO get real headers
-      headers: {
+      headers: Object.assign(this.get('headers'), {
         'profile-id': '8856379a-a8c9-402d-b1b0-faae3b36d137',
         'lms-id': 'its_learning'
-      }
+      })
     };
     const resourceIdParam = resourceId || previousResource.resourceId;
     const url = `${namespace}/${contextId}/event/on-resource/${resourceIdParam}`;
     return Ember.$.ajax(url, options);
   },
 
+  /**
+   * Send event to notify the student submitted all questions in an assignment
+   * @param {String} contextId
+   * @returns {Promise}
+   */
   sendFinishContextEvent: function(contextId) {
     const namespace = this.get('namespace');
     const url = `${namespace}/${contextId}/event/finish`;
@@ -98,15 +134,19 @@ export default Ember.Object.extend({
       contentType: 'application/json; charset=utf-8',
       processData: false,
       data: JSON.stringify({}),
-      // TODO get real headers
-      headers: {
+      headers: Object.assign(this.get('headers'), {
         'profile-id': '8856379a-a8c9-402d-b1b0-faae3b36d137',
         'lms-id': 'its_learning'
-      }
+      })
     };
     return Ember.$.ajax(url, options);
   },
 
+  /**
+   * Send event to notify the student started an assignment
+   * @param {String} contextId
+   * @returns {Promise}
+   */
   sendStartContextEvent: function(contextId) {
     const namespace = this.get('namespace');
     const url = `${namespace}/${contextId}/event/start`;
@@ -116,16 +156,21 @@ export default Ember.Object.extend({
       dataType: 'json',
       processData: false,
       data: JSON.stringify({}),
-      // TODO get real headers
-      headers: {
+      headers: Object.assign(this.get('headers'), {
         'profile-id': '8856379a-a8c9-402d-b1b0-faae3b36d137',
         'lms-id': 'its_learning'
-      }
+      })
     };
     return Ember.$.ajax(url, options);
   },
 
-  updateContext: function(data,contextId) {
+  /**
+   * Update an assignment/context
+   * @param {Object} data
+   * @param {String} contextId
+   * @returns {Promise}
+   */
+  updateContext: function(data, contextId) {
     const namespace = this.get('namespace');
     const url = `${namespace}/${contextId}`;
     const options = {
@@ -134,11 +179,10 @@ export default Ember.Object.extend({
       dataType: 'json',
       processData: false,
       data: JSON.stringify(data),
-      // TODO get real headers
-      headers: {
+      headers: Object.assign(this.get('headers'), {
         'profile-id': '7973e6c6-f0cd-4021-9c49-a0f9f26101b2',
         'lms-id': 'its_learning'
-      }
+      })
     };
     return Ember.$.ajax(url, options);
   }

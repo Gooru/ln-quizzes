f86c9699a8202ad9ee4c18983a0142f81639c417
Melany Delgado <melany.delgado@edify.cr>
2017-02-16 10:51:30 -0600

    Add unit test to Fill in the blank component

--- tests/unit/components/reports/assessments/questions/qz-fib-test.js
+++ tests/unit/components/reports/assessments/questions/qz-fib-test.js
@@ -1,5 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import Ember from 'ember';
+import AnswerModel from 'quizzes/utils/question/answer-object';
+import ResourceModel from 'quizzes/models/resource/resource';
 
 moduleForComponent('reports/assessment/questions/qz-fib', 'Unit | Component | reports/assessment/questions/qz-fib', {
   integration: false
@@ -20,15 +22,131 @@ test('mergeArrays', function (assert) {
   ]);
 
   var merge = component.mergeArrays(sentences, answers);
-  assert.equal(merge.length, 4, "Should have 4 items, empty items are excluded");
-  assert.equal(merge[0].get("text"), 'The mountain is', "Wrong item text");
-  assert.equal(merge[0].get("class"), "sentence", "Wrong item class");
-  assert.equal(merge[1].get("text"), 'green', "Wrong item text");
-  assert.equal(merge[1].get("class"), "answer correct", "Wrong item class");
-  assert.equal(merge[2].get("text"), 'and the sky', "Wrong item text");
-  assert.equal(merge[2].get("class"), "sentence", "Wrong item class");
-  assert.equal(merge[3].get("text"), 'blue', "Wrong item text");
-  assert.equal(merge[3].get("class"), "answer incorrect", "Wrong item class");
+  assert.equal(merge.length, 4, 'Should have 4 items, empty items are excluded');
+  assert.equal(merge[0].get('text'), 'The mountain is', 'Wrong item text');
+  assert.equal(merge[0].get('class'), 'sentence', 'Wrong item class');
+  assert.equal(merge[1].get('text'), 'green', 'Wrong item text');
+  assert.equal(merge[1].get('class'), 'answer correct', 'Wrong item class');
+  assert.equal(merge[2].get('text'), 'and the sky', 'Wrong item text');
+  assert.equal(merge[2].get('class'), 'sentence', 'Wrong item class');
+  assert.equal(merge[3].get('text'), 'blue', 'Wrong item text');
+  assert.equal(merge[3].get('class'), 'answer incorrect', 'Wrong item class');
 
 });
+
+test('answers show correct answer', function(assert) {
+  let component = this.subject();
+  var question = Ember.Object.create({
+    question:ResourceModel.create({
+      questionType: 'text_entry',
+      body: 'The mountain is []',
+      answers:Ember.A([
+        AnswerModel.create({
+          value: 'green',
+          text: 'green'
+        })
+      ]),
+      correctAnswer:Ember.A([
+        AnswerModel.create({ value: 'green'})
+      ]),
+      sequence:1
+    })
+  });
+  component.set('question',question);
+  component.set('showCorrect', true);
+  assert.equal(component.get('answer')[0].text,'The mountain is ','Incorrect text');
+  assert.equal(component.get('answer')[0].class,'sentence','Incorrect class');
+  assert.equal(component.get('answer')[1].text,'green','Incorrect text');
+  assert.equal(component.get('answer')[1].class,'answer correct','Incorrect class,should be correct');
+});
+
+test('answers show user answer anonymous', function(assert) {
+  let component = this.subject();
+  var question = Ember.Object.create({
+    question:ResourceModel.create({
+      questionType: 'text_entry',
+      body: 'The mountain is []',
+      answers:Ember.A([
+        AnswerModel.create({
+          value: 'green',
+          text: 'green'
+        })
+      ]),
+      correctAnswer:Ember.A([
+        AnswerModel.create({ value: 'green'})
+      ]),
+      sequence:1
+    })
+  });
+  var userAnswer = Ember.A([{value:'green'}]);
+
+  component.set('question',question);
+  component.set('userAnswer', userAnswer);
+  component.set('anonymous', true);
+
+  assert.equal(component.get('answer')[0].text,'The mountain is ','Incorrect text');
+  assert.equal(component.get('answer')[0].class,'sentence','Incorrect class');
+  assert.equal(component.get('answer')[1].text,'green','Incorrect text');
+  assert.equal(component.get('answer')[1].class,'answer anonymous','Class should be anonymous');
+});
+
+test('answers show user answer correct', function(assert) {
+  let component = this.subject();
+  var question = Ember.Object.create({
+    question:ResourceModel.create({
+      questionType: 'text_entry',
+      body: 'The mountain is []',
+      answers:Ember.A([
+        AnswerModel.create({
+          value: 'green',
+          text: 'green'
+        })
+      ]),
+      correctAnswer:Ember.A([
+        AnswerModel.create({ value: 'green'})
+      ]),
+      sequence:1
+    })
+  });
+  var userAnswer = Ember.A([{value:'green'}]);
+
+  component.set('question',question);
+  component.set('userAnswer', userAnswer);
+
+  assert.equal(component.get('answer')[0].text,'The mountain is ','Incorrect text');
+  assert.equal(component.get('answer')[0].class,'sentence','Incorrect class');
+  assert.equal(component.get('answer')[1].text,'green','Incorrect text');
+  assert.equal(component.get('answer')[1].class,'answer correct','Class should be correct');
+});
+
+test('answers show user answer incorrect', function(assert) {
+  let component = this.subject();
+  var question = Ember.Object.create({
+    question:ResourceModel.create({
+      questionType: 'text_entry',
+      body: 'The mountain is []',
+      answers:Ember.A([
+        AnswerModel.create({
+          value: 'green',
+          text: 'green'
+        })
+      ]),
+      correctAnswer:Ember.A([
+        AnswerModel.create({ value: 'green'})
+      ]),
+      sequence:1
+    })
+  });
+  var userAnswer = Ember.A([{value:'blue'}]);
+
+  component.set('question',question);
+  component.set('userAnswer', userAnswer);
+
+  assert.equal(component.get('answer')[0].text,'The mountain is ','Incorrect text');
+  assert.equal(component.get('answer')[0].class,'sentence','Incorrect class');
+  assert.equal(component.get('answer')[1].text,'blue','Incorrect text');
+  assert.equal(component.get('answer')[1].class,'answer incorrect','Class should be incorrect');
+});
+
+
 

e6a87f3c808a91cc5587fd6cff4c037a310659ec
gmmendezp <gmmendezp@gmail.com>
2017-05-23 18:33:12 -0600

    NILE-1084. Fix link out resources in resource player.

--- addon/routes/resource-player.js
+++ addon/routes/resource-player.js
@@ -16,27 +16,52 @@ export default Ember.Route.extend({
    */
   quizzesResourceService: Ember.inject.service('quizzes/resource'),
 
+  /**
+   * @type {CollectionService} profileService
+   * @property {Ember.Service} Service to retrieve a profile
+   */
+  profileService: Ember.inject.service('quizzes/profile'),
+
   // -------------------------------------------------------------------------
   // Methods
 
   /**
    * @param {{ contextId: string }} params
    */
-   model(params) {
-     return this.quizzesModel(params);
-   },
+  model(params) {
+    return this.quizzesModel(params);
+  },
 
-   /**
-    * @param {{ contextId: string }} params
-    */
-   quizzesModel(params) {
-     const resourceId = params.resourceId;
-     return Ember.RSVP.hash({
-       resource: this.get('quizzesResourceService').readResource(resourceId)
-     });
-   },
+  /**
+   * @param {{ contextId: string }} params
+   */
+  quizzesModel(params) {
+    const resourceId = params.resourceId;
+    return this.get('quizzesResourceService').readResource(resourceId).then(resource =>
+      Ember.RSVP.hash({
+        resource,
+        owner: this.getOwnerProfile(resource)
+      })
+    );
+  },
+
+  /**
+   * Find owner profile if the resource has narration or is a link out resource
+   */
+  getOwnerProfile: function(resource) {
+    const component = this;
+    let promise = Ember.RSVP.resolve();
+    let resourceId = resource.ownerId;
+    if(resource.get('narration') || resource.get('displayGuide')){
+      let profiles = [resourceId];
+      promise = component.get('profileService').readProfiles(profiles)
+        .then(result => resource.set('owner', result[resourceId]));
+    }
+    return promise;
+  },
 
   setupController(controller, model) {
+    model.resource.set('owner', model.owner);
     controller.set('resource', model.resource);
   }
 });

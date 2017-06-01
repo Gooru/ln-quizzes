61f9661442daa7d813e4ccd7534b76b9e1b1d267
Martin Mendez <gmmendezp@users.noreply.github.com>
2017-06-01 17:23:57 -0600

    Delete test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-var countAnswers = () => +$('.accuracy .text-white-24').text()
-var cleanText = text => text.replace(/[\.\s\t\-]/gi, '').normalize('NFKD').replace(/[\u0300-\u036f]/g, '')
-var picture = cleanText(/url\("https:\/\/officevibe\.blob\.core\.windows\.net\/img\/user-resized\/(.+)_w220_h220\.jpg"\)/.exec($('.flip-card-front .card-content').css('background-image'))[1])
-var answer = $('.row.answer .choice-label').toArray().map((val, i) => ({
-  index: i,
-  name: cleanText($(val).text()),
-  component: $(val)
-})).filter(val => new RegExp(val.name, 'i').test(picture))[0]

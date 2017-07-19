8d6e8f809e8563ccfe466a86ee81ae97293c345d
Melany Delgado <melany.delgado@edify.cr>
2017-07-19 16:46:00 -0600

    NILE-1444:0.11.3 QA [Student: DCA] Cancel assessment start takes user to IL Assessment tab

--- addon/components/player/qz-player-confirmation.js
+++ addon/components/player/qz-player-confirmation.js
@@ -17,10 +17,6 @@ export default Ember.Component.extend({
     }
   },
 
-  didInsertElement: function(){
-    this.get('context');
-  },
-
   // -------------------------------------------------------------------------
   // Properties
 

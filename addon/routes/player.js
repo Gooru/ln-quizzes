21d12c23d78c3ecaae88253cce0816b218771bcd
sheeban <sheeban.s@bambeeq.com>
2017-09-13 17:23:45 +0600

    Nile: 1699, Update UI fixes

--- addon/routes/player.js
+++ addon/routes/player.js
@@ -52,6 +52,13 @@ export default Ember.Route.extend({
     },
 
     /**
+     * Action triggered when the user select resource items from  player
+     */
+    onSelectNavigatorItem: function() {
+      // Empty, it does nothing by default
+    },
+
+    /**
      * Action triggered when the user hits remix on the content player
      */
     onRemixCollection: function() {

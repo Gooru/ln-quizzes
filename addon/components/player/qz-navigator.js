a38797343c74e663e1aa1ae610fd94546c62d62b
Sushma <sushma@gooru.org>
2017-09-15 08:53:03 +0530

    reverted 1605 fix

--- addon/components/player/qz-navigator.js
+++ addon/components/player/qz-navigator.js
@@ -91,6 +91,21 @@ export default Ember.Component.extend(ModalMixin, {
   onFinishCollection: null,
 
   /**
+   * Indicates if the student is playing the collection
+   * @property {boolean}
+   */
+  isStudent: Ember.computed.equal('role', 'student'),
+  /**
+   * Indicates if the teacher is playing this collection
+   * @property {boolean}
+   */
+  isTeacher: Ember.computed.not('isStudent'),
+  /**
+   * Indicates if the teacher is playing this collection
+   * @property {boolean}
+   */
+  showRemix: Ember.computed.and('showBackButton', 'isTeacher'),
+  /**
    * @property {string|function} onItemSelected - event handler for when an item is selected
    */
   onItemSelected: null,

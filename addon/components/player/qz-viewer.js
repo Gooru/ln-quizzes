edcc3c6886f044202ee270fa5d8dadbb223c90a5
sheeban <sheeban.s@bambeeq.com>
2017-11-28 18:36:48 +0600

    golive fixes

--- addon/components/player/qz-viewer.js
+++ addon/components/player/qz-viewer.js
@@ -73,9 +73,24 @@ export default Ember.Component.extend({
     'resource.id',
     'resourceResult.submitted',
     function() {
-      let i18nKey = 'common.next';
-      if (this.get('collection').isLastResource(this.get('resource'))) {
-        i18nKey = 'common.submitAll';
+      let i18nKey = 'common.save-next';
+      const showFeedback =
+        this.get('collection.showFeedback') ===
+        ASSESSMENT_SHOW_VALUES.IMMEDIATE;
+      if (!showFeedback || this.get('isTeacher')) {
+        if (this.get('collection').isLastResource(this.get('resource'))) {
+          i18nKey = this.get('collection').get('isAssessment')
+            ? 'common.save-submit'
+            : 'common.save-finish';
+        }
+      } else {
+        if (this.get('resourceResult.submitted')) {
+          i18nKey = this.get('collection').isLastResource(this.get('resource'))
+            ? 'common.finish'
+            : 'common.next';
+        } else {
+          i18nKey = 'common.submit';
+        }
       }
       return i18nKey;
     }
@@ -108,18 +123,9 @@ export default Ember.Component.extend({
     'resource.id',
     'resourceResult.submitted',
     function() {
-      let i18nKey = 'common.save-next';
-      const showFeedback =
-        this.get('collection.showFeedback') ===
-        ASSESSMENT_SHOW_VALUES.IMMEDIATE;
-      if (!showFeedback) {
-        if (this.get('collection').isLastResource(this.get('resource'))) {
-          return this.get('collection').get('isAssessment')
-            ? 'common.save-submit'
-            : 'common.save-finish';
-        }
-      } else {
-        i18nKey = 'common.submit';
+      let i18nKey = 'common.next';
+      if (this.get('collection').isLastResource(this.get('resource'))) {
+        i18nKey = 'common.submitAll';
       }
       return i18nKey;
     }

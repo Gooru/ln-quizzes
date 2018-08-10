d1f9dc44cd167b891283a319d37d58e8a3f2c554
sheeban <sheeban@gooru.org>
2018-08-10 17:53:12 +0530

    Test case fixes

--- addon/components/reports/qz-assessment-report.js
+++ addon/components/reports/qz-assessment-report.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
-import TaxonomyTag from 'gooru-web/models/taxonomy/taxonomy-tag';
-import TaxonomyTagData from 'gooru-web/models/taxonomy/taxonomy-tag-data';
+import TaxonomyTag from 'quizzes-addon/models/taxonomy/taxonomy-tag';
+import TaxonomyTagData from 'quizzes-addon/models/taxonomy/taxonomy-tag-data';
 
 export default Ember.Component.extend({
   // -------------------------------------------------------------------------

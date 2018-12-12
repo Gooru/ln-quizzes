e2c7086240c0db60071136aa2e7b4113e3faf23f
Ajay <ajaykumar.s@bambeeq.com>
2018-12-12 17:16:11 +0530

    Merge branch 'develop' of https://github.com/Gooru/quizzes into RWDNILE-6

--- addon/components/player/qz-player.js
+++ addon/components/player/qz-player.js
@@ -470,6 +470,7 @@ export default Ember.Component.extend(ModalMixin, {
         )
         .then(
           function() {
+            Ember.run(() => component.set('resource', null));
             resourceResult = contextResult.getResultByResourceId(resourceId);
             resourceResult.set('startTime', new Date().getTime());
             component.setProperties({
